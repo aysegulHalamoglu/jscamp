ae2c8cd9f79073e86e50c49e75180c375ed82c80
Aysegul H <ahalamoglu19@gmail.com>
2021-10-19 00:08:12 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba Kodlama.io");
+
+// js type safe değildir
+let dolarBugun = 9.3;
+let dolarDun = 9.2;
+
+{
+  let dolarDun = 9.1;
+}
+
+console.log(dolarDun);
+
+const euroDun = 11.2;
+console.log(euroDun);
+
+// array
+//camelCasing
+//arrayler çoğul isimlendirilir
+
+let konutKredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  "Özel Konut Kredisi",
+];
+
+console.log("<ul>");
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
+}
+console.log("</ul>");
+console.log(konutKredileri);
